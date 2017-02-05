eb95f4d775ce8ae065077ad94e3750eb7e372737
pxai <pello_altadill@cuatrovientos.org>
2017-02-05 19:20:14 +0100

    Adapted all classes for new provider URL

---
+++ app/src/main/java/io/pello/android/cuatrovendo/ArticleApiClient.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package io.pello.android.cuatrovendo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+/**
+ * @author PELLO_ALTADILL
+ */
+public interface ArticleApiClient {
+	
+	@Headers("Accept: application/json")
+	@GET("/2dam-project-multisite/web/app_dev.php/admin/api/article")
+    Call<List<Article>> articles();
+	
+	@Headers("Accept: application/json")
+	@GET("/2dam-project-multisite/web/app_dev.php/admin/api/article/detail/{id}")
+    Call<Article> article(
+			@Path("id") Long id);
+	
+	@Headers("Accept: application/json")
+	@POST("/2dam-project-multisite/web/app_dev.php/admin/api/article/create")
+    Call<Void> create(@Body ArticlePost article);
+	
+	@PUT("/2dam-project-multisite/web/app_dev.php/admin/api/article/update")
+    Call<Void> update(Article article);
+	
+	@DELETE("3"
+			+ "/api/article/delete/{id}")
+    Call<Void> delete(
+			@Path("id") Long id);
+}
